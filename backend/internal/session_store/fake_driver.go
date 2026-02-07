31dd5e34e164a4acf6896042d5934dc4f156371f
ankumeah <ankitku00ish@gmail.com>
2026-02-08 02:21:10 +0530

    finished backend/internal/session_store/

---
+++ backend/internal/session_store/fake_driver.go
@@ -0,0 +1,37 @@
+package session_store
+
+import (
+  "context"
+  "time"
+)
+
+type fake_driver struct {
+  db map[string]string
+}
+
+func Get_Fake_Driver(ctx context.Context, url string) (*Session_store, error) {
+  return &Session_store { &fake_driver{ map[string]string{} } }, nil
+}
+
+func (r *fake_driver) Get(ctx context.Context, key string) (string, error) {
+  res, ok := r.db[key]
+  if !ok  {
+    return "", Error_not_found
+  }
+
+  return res, nil
+}
+
+func (r *fake_driver) SetEx(ctx context.Context, key string, value string, expiration time.Duration) error {
+  r.db[key] = value
+
+  return nil
+}
+
+func (r *fake_driver) Del(ctx context.Context, keys ...string) (int64, error) {
+  for _, key := range keys {
+    r.db[key] = ""
+  }
+
+  return int64(len(keys)), nil
+}
---
+++ backend/internal/session_store/session_test.go
@@ -0,0 +1,62 @@
+package session_store_test
+
+import (
+	"github.com/Ankumeah/DeltaBase/internal/session_store"
+
+	"context"
+	"testing"
+)
+
+var Ctx = context.Background()
+
+func Test_Add_Session(t *testing.T) {
+  driver, err := session_store.Get_Fake_Driver(Ctx, "test")
+  if err != nil {
+    t.Errorf("Error: %v", err.Error())
+  }
+
+  token, err := driver.Add_Session(Ctx, "test")
+  if err != nil {
+    t.Errorf("Error: %v", err.Error())
+  }
+
+  t.Logf("Resulting token: %v", token)
+}
+
+func Test_Validate_Session(t *testing.T) {
+  driver, err := session_store.Get_Fake_Driver(Ctx, "test")
+  if err != nil {
+    t.Errorf("Error: %v", err.Error())
+  }
+
+  token, err := driver.Add_Session(Ctx, "test")
+  if err != nil {
+    t.Errorf("Error: %v", err.Error())
+  }
+
+  valid, err := driver.Validate_Session(Ctx, "test", token)
+  if err != nil {
+    t.Errorf("Error: %v", err.Error())
+  } else if !valid {
+    t.Errorf("Invalid token")
+  }
+
+  t.Logf("Resulting token: %v", token)
+}
+
+func Test_Delete_Session(t *testing.T) {
+   driver, err := session_store.Get_Fake_Driver(Ctx, "test")
+  if err != nil {
+    t.Errorf("Error: %v", err.Error())
+  }
+
+  _, err = driver.Add_Session(Ctx, "test")
+  if err != nil {
+    t.Errorf("Error: %v", err.Error())
+  }
+
+  err = driver.Delete_Session(Ctx, "test")
+  if err != nil {
+    t.Errorf("Error: %v", err.Error())
+  }
+}

890c789d4836dda8b0df1790de91003414bf858b
ankumeah <ankitku00ish@gmail.com>
2026-05-22 01:18:03 +0530

    fix(backend): fixed backends writing to local filesystem

--- backend/internal/git/types.go
+++ backend/internal/git/types.go
@@ -1,15 +1,20 @@
 package git
 
-import (
-  "path"
-)
+import "path"
 
 const max_blob_size = 2097152
 
 var base_path string
 
 func init() {
-  base_path = path.Join("srv", "git")
+  // This breaks on windows,
+  // Im sorry if you are running this on bare
+  // metal on windows but if you are then you are
+  // kinda asking for it.
+  // The fact that you are reading this means you
+  // can probably change this on your own.
+  // Good luck and apologies from my side
+  base_path = path.Join("/srv", "git")
 }
 
 type File struct {

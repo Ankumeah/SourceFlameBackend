4526d4400712145876f2e84841ead19d4e268612
ankumeah <ankitku00ish@gmail.com>
2026-05-22 01:25:20 +0530

    chore(k8s): updated k8s files to support multiple DBs

--- database/run_postgres.sh
+++ database/run_postgres.sh
@@ -4,6 +4,5 @@ export POSTGRES_USER=$(echo "$DATABASE_URL" | awk -F/ '{print $3}' | awk -F: '{p
 export POSTGRES_PASSWORD=$(echo "$DATABASE_URL" | awk -F/ '{print $3}' | awk -F: '{print $2}' | awk -F@ '{print $1}')
 export POSTGRES_PORT=$(echo "$DATABASE_URL" | awk -F/ '{print $3}' | awk -F: '{print $3}')
 export POSTGRES_DB=$(echo "$DATABASE_URL" | awk -F/ '{print $4}' | awk -F\? '{print $1}')
-echo POSTGRES_DB $POSTGRES_DB POSTGRES_PORT $POSTGRES_PORT POSTGRES_PASSWORD $POSTGRES_PASSWORD POSTGRES_USER $POSTGRES_USER
 
 exec docker-entrypoint.sh postgres
